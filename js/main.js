91913395cf56dc5dab18fd25d18756ddd64de24d
Krelle156 <kotofteng@gmail.com>
2025-11-18 13:46:34 +0100

    Sorting and made deletecar not disturb the chosen sorting.

--- js/main.js
+++ js/main.js
@@ -5,13 +5,25 @@ const app = Vue.createApp({
         return {
             intro: 'Welcome to my Vue template',
             carList:[],
+            allCarList:[],
             newCar: { vendor: '', model: '', price: 0},
-            statusCode: null
+            statusCode: null,
+            asc: false
         }
     },
+    created() {
+        this.getAllCars()
+    },
     methods: {
-        myMethod(){
-
+        sortByPrice(){
+            if(this.asc){
+                this.carList = this.allCarList
+                this.carList.sort((a, b) => b.price - a.price);
+            } else {
+                this.carList = this.allCarList
+                this.carList.sort((a, b) => a.price - b.price);
+            }
+            this.asc = !this.asc
         },
         postCar() {
             axios.post(baseUrl, this.newCar)
@@ -36,12 +48,14 @@ const app = Vue.createApp({
         },
         getAllCars(){
             console.log("er i metoden getAllCars");
+            this.asc = false
 
             axios.get(baseUrl)
             .then(
                 response => {
                     console.log(response)
                     this.carList = response.data
+                    this.allCarList = response.data
                     this.statusCode = response.status
                 }
             )
@@ -60,7 +74,8 @@ const app = Vue.createApp({
                 response => {
                     console.log(response)
                     this.statusCode = response.status
-                    this.getAllCars()
+                    this.carList = this.carList.filter(c => c.id !== id)
+                    this.allCarList = this.allCarList.filter(c => c.id !== id)
                 }
             )
             .catch(
